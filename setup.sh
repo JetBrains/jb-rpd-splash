61e87ead854ceed57a35af47f7d9758ef7f5a2e5
Ulric Wilfred <shaman.sir@gmail.com>
2016-11-18 02:19:47 +0100

    add basic structure, improve setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-cd ../rpd
+
+cd ..
+
+if [ ! -d ./rpd ]; then
+    git clone git@github.com:shamansir/rpd.git
+fi
+
+cd ./rpd
 gulp -r svg -s plain -o ../jb-rpd-splash
 cd ../jb-rpd-splash
+
+if [ ! -f ./kefir.min.js ]; then
+    wget http://rawgit.com/rpominov/kefir/gh-pages/dist/kefir.min.js
+fi
