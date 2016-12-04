7df44f6526b5e672453bf00258be04549801ba2a
Ulric Wilfred <shaman.sir@gmail.com>
2016-12-04 22:41:57 +0100

    fix syntax issue

--- toolkit/jb/toolkit.js
+++ toolkit/jb/toolkit.js
@@ -172,7 +172,7 @@ Rpd.nodetype('jb/image', {
                                 height: height,
                                 values: pixels
                             });
-                        }
+                        });
                     }
                 } ]
                 : [],
